14fadd1185df1e5532eabd126ea9675f0cbe2e9e
KarlStevenRequinto <lerdrobott@gmail.com>
2025-03-26 21:07:49 +0800

    adding exchanges,linechart,loader component

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsHeaders = {
-    "x-bingapis-sdk": "true",
+    "X-BingApis-SDK": "true",
     "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
     "x-rapidapi-host": process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
 };
